db2e9b7976959fe830dcbca50b1b5490009f72ec
thomasivchin <78983683+thomasivchin@users.noreply.github.com>
2022-09-05 18:09:42 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Global-Med-Tour-Landing
---
+++ src/main.js
@@ -0,0 +1,8 @@
+
+"use strict";
+$(function (e) {
+ $('.confidence_politic, .closeBtn').click(function (e) {
+  $('.modal_box').toggleClass('open');
+ })
+})
+
